89ea5698d55bd7ba6e60ceb922488429e26c5a3d
Pranjal Arora <pranjal.arora@gdn-commerce.com>
2022-01-22 22:36:38 +0530

    Changes

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FaceBook/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import FaceBook from '@/components/FaceBook.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    FaceBook
   }
 }
 </script>
@@ -23,6 +22,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: #f0f2f5;
 }
 </style>
---
+++ src/components/FaceBook.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="facebook">
+    <div class="facebook__left">
+      <div class="input">
+  <input class="email" placeholder="Email address or phone number"/>
+      </div>
+      <div class="input">
+   <input class="password" placeholder="Password"/>
+      </div>
+      <div class="button">
+      <button>Log In</button>
+      </div>
+      <div class="forgot">
+        Forgotten password?
+      </div>
+      <div class="create-account">
+        <button>
+        Create New Account
+        </button>
+      </div>
+    </div>
+     <div class="facebook__right">
+       <div class="facebook__right__upper"><img src="https://static.xx.fbcdn.net/rsrc.php/y8/r/dF5SId3UHWd.svg"/></div>
+       <div class="facebook__right__lower">{{text}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FaceBook',
+  data(){
+    return {
+      text:{}
+    }
+  },
+  methods:{
+    getData() {
+      let req= new XMLHttpRequest()
+      let url="https://jsonplaceholder.typicode.com/posts/1"
+      req.open("GET",url)
+      req.send()
+      req.onreadystatechange=()=>{
+      this.text=JSON.parse(req.responseText).body
+      }
+    }
+  },
+  mounted(){
+    this.getData()
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import '@/mixins/responsive';
+.facebook{
+  width:100vw;
+  height:100vh;
+  padding:20px;
+  display:flex;
+  padding-top:106px;
+  &__left {
+    height:394px;
+    width:396px;
+    background-color:white;
+    border-radius:8px;
+    padding:20px;
+    .input {
+      width:358px;
+      .email{
+        margin-bottom:8px;
+      }
+    input{
+      width:100%;
+      padding:14px;
+      border: 1px solid #dddfe2;
+      border-radius: 4px;
+      font-size: 17px;
+    }
+    }
+    .button {
+      margin-top:8px;
+      button{
+        background-color: #1877f2;
+        border-radius: 6px;
+        color:white;
+        padding: 20px;
+        width:100%;
+        font-size: 16px;
+        border:none;
+        font-weight: bold;
+      }
+    }
+    .forgot{
+          color: #1877f2;
+    font-size: 14px;
+    font-weight: 500;
+    margin-top:16px;
+    border-bottom: 1px solid #dadde1;
+    padding-bottom: 40px;
+    }
+    .create-account {
+      margin-top:6px;
+     button {
+       color:white;
+      background-color: #42b72a;
+      border:none;
+      border-radius: 6px;
+      padding:16px;
+      font-size: 20px;
+      font-weight: bold;
+     }
+    }
+  }
+  &__right{
+        text-align: left;
+    margin-left: 45px;
+    width: 700px;
+    &__upper{
+     width: 280px;
+    }
+  }
+}
+</style>
